5fe406dafa08079632f11aa3e82287a5e2c30138
JAcciarri <acciarrijoshua@gmail.com>
2025-05-29 09:38:48 -0300

    Inicio de API Testing, decidi RestAssured como herramienta
    Decidi usar rest assured por su facilidad de implementacion y los metodos utiles que tiene para hacer diferentes llamadas y validar las responses

---
+++ src/test/java/api/services/UserApiService.java
@@ -0,0 +1,6 @@
+package api.services;
+
+public class UserApiService {
+
+    // Servicio para encapsular los metodos que se piden de get, post, getAll
+}
---
+++ src/test/java/api/tests/UserApiTest.java
@@ -0,0 +1,18 @@
+package api.tests;
+
+import org.testng.annotations.Test;
+
+public class UserApiTest {
+
+
+    // Podria implementar un data provider para pasar los datos de los usuarios
+    @Test
+    public void testGetUserById(){
+
+    }
+    @Test
+    public void testGetAllUsers(){
+
+    }
+
+}
