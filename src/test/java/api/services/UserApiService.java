2b8e77d99b7108f8cdd56bfa8566b574086525e1
JAcciarri <acciarrijoshua@gmail.com>
2025-05-29 10:23:52 -0300

    Tests Basicos de API - Obtener lista de usuarios, detalle especifico de un Usuario

--- src/test/java/api/services/UserApiService.java
+++ src/test/java/api/services/UserApiService.java
@@ -1,6 +1,34 @@
 package api.services;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class UserApiService {
 
     // Servicio para encapsular los metodos que se piden de get, post, getAll
+    private final String BASE_URL = "https://gorest.co.in/public/v2";
+    private final String TOKEN = "Bearer " + System.getenv("API_TOKEN");
+
+    public UserApiService() {
+        RestAssured.baseURI = BASE_URL;
+    }
+
+    public Response getUsers() {
+        return RestAssured
+                .given()
+                .get("/users");
+    }
+    public Response getUserById(int userId) {
+        return RestAssured
+                .given()
+                .get("/users/" + userId);
+    }
+
+    public int getExistingUserId(){
+        Response response = getUsers();
+        if (response.getStatusCode() == 200 && response.jsonPath().getList("id").size() > 0) {
+            return (int) response.jsonPath().getList("id").get(1);
+        }
+        throw new RuntimeException("No users found or failed to retrieve users.");
+    }
+
 }
--- src/test/java/api/tests/UserApiTest.java
+++ src/test/java/api/tests/UserApiTest.java
@@ -1,17 +1,50 @@
 package api.tests;
 
+import api.services.UserApiService;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utils.LoggerUtil;
+
 
 public class UserApiTest {
 
+    Logger logger = LoggerUtil.getLogger(UserApiTest.class);
 
     // Podria implementar un data provider para pasar los datos de los usuarios
     @Test
     public void testGetUserById(){
+        SoftAssert sAssert = new SoftAssert();
+        UserApiService userService = new UserApiService();
+        final int userId = userService.getExistingUserId();
+
+        Response userDetail = userService.getUserById(userId);
+        sAssert.assertEquals(userDetail.getStatusCode(), 200, "Status code is not 200");
+        sAssert.assertTrue(userDetail.getBody().asString().contains("id"), "Response body does not contain user id");
+        sAssert.assertEquals((int)userDetail.getBody().jsonPath().get("id"), userId, "User id does not match");
+        sAssert.assertTrue(userDetail.getBody().asString().contains("name"), "Response body does not contain user name");
+        sAssert.assertTrue(userDetail.getBody().asString().contains("email"), "Response body does not contain user email");
+        // Listar el detalle especifico del usuario
+        logger.info("User details in JSON: {}", userDetail.getBody().asString());
+        userDetail.jsonPath().getMap("").forEach(
+                (key, value) -> logger.info("User {}: {}", key, value)
+        );
+        sAssert.assertAll();
 
     }
     @Test
     public void testGetAllUsers(){
+        SoftAssert sAssert = new SoftAssert();
+        UserApiService userService = new UserApiService();
+        Response response = userService.getUsers();
+        sAssert.assertEquals(response.getStatusCode(), 200, "Status code is not 200");
+        System.out.println(response.getBody().asString());
+        sAssert.assertTrue(response.getBody().asString().contains("id"), "Response body does not contain any user id");
+        sAssert.assertTrue(response.getBody().asString().contains("name"), "Response body does not contain any user name");
+        sAssert.assertTrue(response.getBody().asString().contains("email"), "Response body does not contain any user email");
+
+        sAssert.assertAll();
 
     }
 
