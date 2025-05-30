c44c5d863ae42af034f1522bf067cccec533ea03
JAcciarri <acciarrijoshua@gmail.com>
2025-05-30 19:42:14 -0300

    BRANCH DE SIMULACION PARA CI/CD

---
+++ src/test/java/simulacion/SimularCambios.java
@@ -0,0 +1,13 @@
+package simulacion;
+
+public class SimularCambios {
+    /*
+    Clase de simulacion de cambios para la branch feature-cuotas.
+    Cualquier PR que se mergee a esta branch va a disparar el workflow configurado en Jenkins
+     */
+
+    String mensaje = "Simulando cambios en la branch feature-cuotas para pruebas de integración continua.";
+    public String getMensaje() {
+        return mensaje;
+    }
+}
