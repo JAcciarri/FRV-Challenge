e2fb72113eff8f22cda96e107d187cde43142362
JAcciarri <acciarrijoshua@gmail.com>
2025-05-31 08:54:10 -0300

    Modificado WebDriver Factory para ejecucion paralela por hilos e integracion con Jenkins

--- src/main/java/webdriver/WebDriverFactory.java
+++ src/main/java/webdriver/WebDriverFactory.java
@@ -15,23 +15,43 @@ public class WebDriverFactory {
 
     private static final Logger logger = LoggerUtil.getLogger(WebDriverFactory.class);
 
-    public static WebDriver createDriver(String browser) {
+    public static WebDriver createDriver(String browser, String executionEnv) {
+        boolean jenkinsRun = "jenkins".equalsIgnoreCase(executionEnv);
+        if(jenkinsRun) {logger.info("Running in Jenkins environment, setting browser to headless mode.");}
+
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--start-maximized");
+                if (jenkinsRun) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    chromeOptions.addArguments("--start-maximized");
+                }
                 return new ChromeDriver(chromeOptions);
 
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--start-maximized");
+                if (jenkinsRun) {
+                    firefoxOptions.addArguments("-headless");
+                    firefoxOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    firefoxOptions.addArguments("--start-maximized");
+                }
                 return new FirefoxDriver(firefoxOptions);
+
             case "edge":
                 WebDriverManager.edgedriver().setup();
                 EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--start-maximized");
+                if (jenkinsRun) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    edgeOptions.addArguments("--start-maximized");
+                }
                 return new EdgeDriver(edgeOptions);
 
             default:
--- src/test/java/fravega/base/ApplicationBaseTest.java
+++ src/test/java/fravega/base/ApplicationBaseTest.java
@@ -5,38 +5,44 @@ import org.testng.ITestContext;
 import webdriver.WebDriverFactory;
 import org.testng.annotations.*;
 
-import java.util.Objects;
-
 public class ApplicationBaseTest {
 
-    protected WebDriver driver;
+    // ThreadLocal para manejar el WebDriver por hilos y permitir ejecucion paralela
+    private static final ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 
     protected WebDriver getDriver() {
+        return threadDriver.get();
+    }
+
+    /*protected WebDriver getDriver() {
         // Devuelve el driver creado por el metodo setup y sino devuelve uno nuevo de chrome con el Factory
-        return Objects.requireNonNullElseGet(driver, () -> WebDriverFactory.createDriver("chrome"));
-    }
+        return Objects.requireNonNullElseGet(driver, () -> WebDriverFactory.createDriver("chrome", "local"));
+    }*/
 
-
-    @BeforeTest(alwaysRun = true)
-    public void setUp(ITestContext context) {
+    @BeforeMethod(alwaysRun = true)
+    public void testSetup(ITestContext context) {
         String browser = context.getCurrentXmlTest().getParameter("browser");
+        String executionEnv = context.getCurrentXmlTest().getParameter("executionEnv");
+
         if (browser == null || browser.isEmpty()) {
-            browser = "chrome"; // Default browser
+            browser = "chrome";
         }
-         driver = WebDriverFactory.createDriver(browser);
+        if (executionEnv == null || executionEnv.isEmpty()) {
+            executionEnv = "local";
+        }
+
+        WebDriver driver = WebDriverFactory.createDriver(browser, executionEnv);
+        threadDriver.set(driver);
     }
 
-    @AfterTest
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        if(driver != null) {
-            driver.close();
-        }
-    }
-    @AfterSuite
-    public void afterClass() {
+        WebDriver driver = threadDriver.get();
         if (driver != null) {
             driver.quit();
+            threadDriver.remove();
         }
     }
+
 }
 
