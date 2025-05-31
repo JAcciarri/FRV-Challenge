dfb7b8db2392095f02bd62f5f75c940e649b84f5
JAcciarri <acciarrijoshua@gmail.com>
2025-05-31 10:05:21 -0300

    Cambios simulados en rama feature-cuotas

--- src/test/java/simulacion/SimularCambios.java
+++ src/test/java/simulacion/SimularCambios.java
@@ -7,6 +7,7 @@ public class SimularCambios {
      */
 
     String mensaje = "Simulando cambios simulados en la branch feature-cuotas para pruebas de integración continua.";
+    String cambio = "";
     public String getMensajeChanged() {
         return mensaje;
     }
