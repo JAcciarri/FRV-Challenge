d2ff947b84c66f417b308b38389307757ffb06f5
JAcciarri <acciarrijoshua@gmail.com>
2025-05-31 12:28:41 -0300

    Cambios

--- src/test/java/simulacion/SimularCambios.java
+++ src/test/java/simulacion/SimularCambios.java
@@ -6,11 +6,12 @@ public class SimularCambios {
     Cualquier PR que se mergee a esta branch va a disparar el workflow configurado en Jenkins
      */
 
-    String mensaje = "Simulando cambios simulados en la branch feature-cuotas para pruebas de integración continua.";
+    String mensaje = "Simulando cambios simulados en la branch feature-cuotas";
     public String getMensajeChanged() {
         return mensaje;
     }
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
+
 }
