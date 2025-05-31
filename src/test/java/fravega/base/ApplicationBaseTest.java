5ce69d0cc0d61af62c0c535cdb6a055ea8f56277
JAcciarri <acciarrijoshua@gmail.com>
2025-05-31 11:56:45 -0300

    ScreenShotListener implementado: reemplaza al util para generar los screenshots

--- src/test/java/fravega/base/ApplicationBaseTest.java
+++ src/test/java/fravega/base/ApplicationBaseTest.java
@@ -1,6 +1,7 @@
 package fravega.base;
 
 import fravega.utils.ScreenshotUtil;
+import fravega.utils.listeners.ScreenshotListener;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +10,14 @@ import org.testng.ITestResult;
 import webdriver.WebDriverFactory;
 import org.testng.annotations.*;
 
+@Listeners({ScreenshotListener.class})
 public class ApplicationBaseTest {
 
     // ThreadLocal para manejar el WebDriver por hilos y permitir ejecucion paralela
     private static final ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
     private static final Logger log = LoggerFactory.getLogger(ApplicationBaseTest.class);
 
-    protected WebDriver getDriver() {
+    public static WebDriver getDriver() {
         WebDriver driver = threadDriver.get();
         if (driver == null) {
             throw new IllegalStateException("WebDriver no ha sido inicializado para el hilo actual.");
